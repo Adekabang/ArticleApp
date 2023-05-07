08442b569b1e13ea4b299b1d1476d6bcc6904aab
Adekabang <iban.ninja@gmail.com>
2023-05-08 00:36:29 +0700

    Initial Commit

---
+++ ArticleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArticleApp
+//
+//  Created by Raska Mohammad on 08/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
