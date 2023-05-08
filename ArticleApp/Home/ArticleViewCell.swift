674b7a443e4964c349750de50611513d85bc4a61
Adekabang <iban.ninja@gmail.com>
2023-05-08 14:35:01 +0700

    create custom cell, and regist object to ArticleViewCell

---
+++ ArticleApp/Home/ArticleViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ArticleViewCell.swift
+//  ArticleApp
+//
+//  Created by Raska Mohammad on 08/05/23.
+//
+
+import UIKit
+
+class ArticleViewCell: UITableViewCell {
+    @IBOutlet weak var thumbImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var bookmarkButton: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
