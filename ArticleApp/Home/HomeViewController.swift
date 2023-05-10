ef5fd47b84b2d5e20b3058426f43743a607255aa
Adekabang <iban.ninja@gmail.com>
2023-05-10 18:42:28 +0700

    enable paging, disable scroll indicator, and set spacing for top article

--- ArticleApp/Home/HomeViewController.swift
+++ ArticleApp/Home/HomeViewController.swift
@@ -74,7 +74,7 @@ extension HomeViewController: UITableViewDataSource {
             cell.pageControl.numberOfPages = latestArticleList.count
             
             cell.collectionView.dataSource = self
-//            cell.collectionView.delegate = self
+            cell.collectionView.delegate = self
             cell.collectionView.reloadData()
             
             return cell
@@ -164,5 +164,25 @@ extension HomeViewController: UICollectionViewDataSource {
         cell.subtitleLabel.text = "\(articleDate) · \(article.source)"
         
         return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+extension HomeViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = UIScreen.main.bounds.width
+        return CGSize(width: width, height: 256)
     }
 }
